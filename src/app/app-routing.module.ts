a0d3675bc33ef086d2d9d522eb72c8d079e5a1ec
JhonTreft <jonz37133@gmail.com>
2023-04-19 20:16:46 -0500

    add routers for the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './adminitration-sc/login/login.component';
-
+import { RegisterComponent } from './adminitration-sc/register/register.component';
 
 const routes: Routes = [
   {
     path:'login',
     component:LoginComponent,
+  },
+  {
+    path:'register',
+    component:RegisterComponent
   }
 ];
 
