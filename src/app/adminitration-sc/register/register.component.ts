52dedf860e45528625cc734f2f48ece47ccb96b9
JhonTreft <jonz37133@gmail.com>
2023-04-19 20:17:17 -0500

    add logic and validations in the component

--- src/app/adminitration-sc/register/register.component.ts
+++ src/app/adminitration-sc/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  emailControl: FormControl;
+  emailForm: FormGroup;
+  passwordControl:FormControl;
 
+  email:string='';
+  password:string='';
+
+
+  constructor() {
+    this.passwordControl = new FormControl('');
+    this.emailControl = new FormControl('', [Validators.required, this.emailValidator]);
+    this.emailForm = new FormGroup({
+      email: this.emailControl,
+      password: this.passwordControl,
+    });
+  }
+
+
+  emailValidator(control: FormControl): { [s: string]: boolean } {
+
+    if (control.value.length > 3) {
+      if (!control.value.match(/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/)) {
+        return { invalidEmail: true };
+      }
+    }
+
+    return {};
+  }
+
+  handleSubmitFormLogin(event: Event) {
+    event.preventDefault();
+    //Se va enviarlos datos para la autenticacion
+
+    this.passwordControl.reset();
+    this.emailControl.reset();
+
+  }
 }
+
