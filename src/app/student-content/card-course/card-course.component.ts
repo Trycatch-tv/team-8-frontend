d6f8a2b7d8d78aeaaff8273136ddbebdf95c7cef
julian moreno <julianmoreno242@gmail.com>
2023-04-23 13:42:45 -0500

    added card-course component

---
+++ src/app/student-content/card-course/card-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-course',
+  templateUrl: './card-course.component.html',
+  styleUrls: ['./card-course.component.css']
+})
+export class CardCourseComponent {
+
+}
