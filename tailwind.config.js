ab82b71a5a411ac6872deb29ff98b46f3c6e79b5
julian moreno <julianmoreno242@gmail.com>
2023-04-23 13:50:14 -0500

    added new styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
       colors:{
         'banner-bg':'#2764E9',
         'banner-gray-bg':'#F3F4F6',
+        'text-card':'#B3CFF2',
+        'text-title-card':'#0455BF'
       },
       height: {
         '300': '300px',
