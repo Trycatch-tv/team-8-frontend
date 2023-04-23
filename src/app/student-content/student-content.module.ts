5bb8844b7e41527348b11e3707ea95a203e711a0
julian moreno <julianmoreno242@gmail.com>
2023-04-23 13:55:39 -0500

    routes add in student module

--- src/app/student-content/student-content.module.ts
+++ src/app/student-content/student-content.module.ts
@@ -2,16 +2,48 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { DetailCourseComponent } from './detail-course/detail-course.component';
+import { ContainerCourseComponent } from './container-course/container-course.component';
+import { CardCourseComponent } from './card-course/card-course.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  {
+    path: 'student',
+    component: HomeComponent,
+    children:[
+      {
+        path:'courses',
+        component:ContainerCourseComponent
+      },
+      {
+      path:'detail-course/:id',
+      component:DetailCourseComponent
+    },
+     
+    {
+      path:'',
+      component:HomepageComponent
+    },
+  ]
+  }
+];
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DetailCourseComponent
+    DetailCourseComponent,
+    ContainerCourseComponent,
+    CardCourseComponent,
+    NavBarComponent,
+    HomepageComponent
   ],
   imports: [
-    CommonModule
+    RouterModule.forChild(routes),
+    CommonModule,
   ]
 })
 export class StudentContentModule { }
