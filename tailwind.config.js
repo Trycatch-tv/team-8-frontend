5216f6b250a451cc677d5e3990aa4ed7f3af4d9a
julian moreno <julianmoreno242@gmail.com>
2023-04-22 20:45:11 -0500

    unused color removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
       colors:{
         'banner-bg':'#2764E9',
         'banner-gray-bg':'#F3F4F6',
-        'eclipse-img':'#695E98',
       },
       height: {
         '300': '300px',
