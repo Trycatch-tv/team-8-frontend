962ecab70fa6aef020bc8b1889e408c894c49fa0
julian moreno <julianmoreno242@gmail.com>
2023-04-24 17:30:35 -0500

    accept change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AdminitrationScModule } from './adminitration-sc/adminitration-sc.module';
 import { SidebarTeacherComponent } from './layout/sidebar-teacher/sidebar-teacher.component';
-<<<<<<< HEAD
-import { StudentContentModule } from './student-content/student-content.module';
-=======
 import { DashboardAdminModule } from './dashboard-admin/dashboard-admin.module';
->>>>>>> cdf22e4b1a3edca84f743c3120af8275898a39a1
 
 @NgModule({
   declarations: [
@@ -24,12 +20,8 @@ import { DashboardAdminModule } from './dashboard-admin/dashboard-admin.module';
     FormsModule,
     ReactiveFormsModule,
     AdminitrationScModule,
-<<<<<<< HEAD
-    StudentContentModule
-=======
     DashboardAdminModule
 
->>>>>>> cdf22e4b1a3edca84f743c3120af8275898a39a1
   ],
   providers: [],
   bootstrap: [AppComponent]
