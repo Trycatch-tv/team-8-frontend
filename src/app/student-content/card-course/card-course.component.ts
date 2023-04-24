dc1de52e9af40db4b31625be359ce323b84206eb
julian moreno <julianmoreno242@gmail.com>
2023-04-24 17:07:59 -0500

    Added prop name route to component with decorator input

--- src/app/student-content/card-course/card-course.component.ts
+++ src/app/student-content/card-course/card-course.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-course',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-course.component.css']
 })
 export class CardCourseComponent {
-
+   @Input() route:string=''
 }
