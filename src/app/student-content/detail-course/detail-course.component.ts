280529c95329160f4153542670e0bdcf54a44c35
julian moreno <julianmoreno242@gmail.com>
2023-04-25 09:04:50 -0500

    variables were added and the parameter of the route was extracted to filter the course to render

--- src/app/student-content/detail-course/detail-course.component.ts
+++ src/app/student-content/detail-course/detail-course.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentData } from '../type';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { StudentService } from 'src/app/services/student/student.service';
 
 @Component({
   selector: 'app-detail-course',
   templateUrl: './detail-course.component.html',
   styleUrls: ['./detail-course.component.css']
 })
-export class DetailCourseComponent {
+export class DetailCourseComponent implements OnInit {
+  private routeSub!: Subscription;
+  constructor(private route: ActivatedRoute,private studentService: StudentService) { }
+  courseDetail!: StudentData | undefined
+  courseID!:string
 
+ ngOnInit(): void {
+   this.routeSub = this.route.params.subscribe((params)=>{
+    this.courseID=params['id']
+   })
+
+   this.studentService.getAllCourses().subscribe((data:any) => {
+    const courses = data as StudentData[]; // convierte los datos a un arreglo de StudentData
+    this.courseDetail = courses.find((course: StudentData) => course.id.toString() === this.courseID.toString());
+  });
+ }
+ 
 }
