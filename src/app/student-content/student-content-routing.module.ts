e03534183b80f94bd5c52f6392a0827cc6cf2f2f
julian moreno <julianmoreno242@gmail.com>
2023-04-25 09:41:56 -0500

    added module for routes of student-content

---
+++ src/app/student-content/student-content-routing.module.ts
@@ -0,0 +1,45 @@
+import { Routes,RouterModule } from "@angular/router";
+import { ContainerCourseComponent } from "./container-course/container-course.component";
+import { DetailCourseComponent } from "./detail-course/detail-course.component";
+import { DetailMyCourseComponent } from "./detail-my-course/detail-my-courses.component";
+import { HomeComponent } from "./home/home.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { MyCoursesComponent } from "./my-courses/my-courses.component";
+import { NgModule } from "@angular/core";
+
+
+const routes: Routes = [
+    {
+      path: 'student',
+      component: HomeComponent,
+      children: [
+        {
+          path: 'courses',
+          component: ContainerCourseComponent,
+        },
+        {
+          path: 'my-courses',
+          component: MyCoursesComponent
+        },
+        {
+          path: 'detail-course/:id',
+          component: DetailCourseComponent,
+        },
+        {
+          path: 'detail-my-course/:id',
+          component: DetailMyCourseComponent
+        },
+        {
+          path: '',
+          component: HomepageComponent
+        },
+      ]
+    }
+  ];
+
+  @NgModule({
+    imports:[RouterModule.forChild(routes)],
+    exports:[RouterModule]
+  })
+
+  export class StudentContentRoutingModule{}
