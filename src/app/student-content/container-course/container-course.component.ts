8eecb53c9540efeb621d72924470979152c22ffa
julian moreno <julianmoreno242@gmail.com>
2023-04-25 10:54:37 -0500

    added feature for search courses

--- src/app/student-content/container-course/container-course.component.ts
+++ src/app/student-content/container-course/container-course.component.ts
@@ -7,18 +7,29 @@ import { StudentData } from '../type';
   styleUrls: ['./container-course.component.css']
 })
 export class ContainerCourseComponent implements OnInit {
-  inputSearchValue:string=''
-  constructor(private studentService: StudentService) {}
+  inputSearchValue: string = ''
+  constructor(private studentService: StudentService) { }
+  coursesAll!: Array<StudentData>
+  courses!: Array<StudentData>
 
-  courses!:Array<StudentData>
- 
 
   ngOnInit() {
     this.studentService.getAllCourses().subscribe((data: any) => {
-      this.courses = data;
+      this.courses = data
+      this.coursesAll = data
     });
   }
+  handleSearch() {
+    if (!this.inputSearchValue) {
+      this.courses = this.coursesAll;
+      return;
+    }
   
+    this.courses=this.coursesAll.filter(item => (
+      item.nombre.toLowerCase().includes(this.inputSearchValue.toLowerCase())
+    ));
+  }
+
 }
 
 
