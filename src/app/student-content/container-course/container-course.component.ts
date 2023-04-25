b31774b62245d07b7ed56d5246d43f690978130e
julian moreno <julianmoreno242@gmail.com>
2023-04-25 08:59:39 -0500

    The typed course variable was added with its interface and the getallcourses service was called

--- src/app/student-content/container-course/container-course.component.ts
+++ src/app/student-content/container-course/container-course.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/student/student.service';
+import { StudentData } from '../type';
 @Component({
   selector: 'app-container-course',
   templateUrl: './container-course.component.html',
   styleUrls: ['./container-course.component.css']
 })
-export class ContainerCourseComponent {
+export class ContainerCourseComponent implements OnInit {
+  inputSearchValue:string=''
+  constructor(private studentService: StudentService) {}
 
+  courses!:Array<StudentData>
+ 
+
+  ngOnInit() {
+    this.studentService.getAllCourses().subscribe((data: any) => {
+      this.courses = data;
+    });
+  }
+  
 }
+
+
+
