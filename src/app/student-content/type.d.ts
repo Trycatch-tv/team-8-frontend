270934511a09d30a053d343efd5a90eec455e72b
julian moreno <julianmoreno242@gmail.com>
2023-04-25 09:22:00 -0500

    added interfaces file

---
+++ src/app/student-content/type.d.ts
@@ -0,0 +1,15 @@
+export interface StudentData{
+
+    id: string
+    nombre: string
+    categoria: string
+    nivel_curso:string
+    numero_max_estudiantes: number
+    codigo_curso:string
+    created_at:string
+    estado: string
+    valoraciones: number
+    id_estudiante: null
+    id_profesor: null
+	
+}
