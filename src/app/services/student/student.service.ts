a2f0955f4fdd420ee5a598af1157e83c551a38b9
julian moreno <julianmoreno242@gmail.com>
2023-04-25 08:53:07 -0500

    added service get all course in student services

--- src/app/services/student/student.service.ts
+++ src/app/services/student/student.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllCourses() {
+    return this.http.get('http://localhost:8000/list_courses/');
+  }
 }
