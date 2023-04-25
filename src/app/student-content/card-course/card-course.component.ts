59a6cebe3a3fbc50a34ebd691c5c5621b31771a4
julian moreno <julianmoreno242@gmail.com>
2023-04-25 09:09:32 -0500

    Variable data was declared with decorator input

--- src/app/student-content/card-course/card-course.component.ts
+++ src/app/student-content/card-course/card-course.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { StudentData } from '../type';
 
 @Component({
   selector: 'app-card-course',
@@ -7,4 +8,5 @@ import { Component, Input } from '@angular/core';
 })
 export class CardCourseComponent {
    @Input() route:string=''
+   @Input() data!:StudentData
 }
