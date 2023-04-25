dcbe42a521ce2f7ee8146725a3ae9926b9469c6b
julian moreno <julianmoreno242@gmail.com>
2023-04-25 09:40:14 -0500

    Removed module routes and imported routing module for student content

--- src/app/student-content/student-content.module.ts
+++ src/app/student-content/student-content.module.ts
@@ -6,43 +6,13 @@ import { ContainerCourseComponent } from './container-course/container-course.co
 import { CardCourseComponent } from './card-course/card-course.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MyCoursesComponent } from './my-courses/my-courses.component';
 import { DetailMyCourseComponent } from './detail-my-course/detail-my-courses.component';
 import { StudentService } from '../services/student/student.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
-const routes: Routes = [
-  {
-    path: 'student',
-    component: HomeComponent,
-    children: [
-      {
-        path: 'courses',
-        component: ContainerCourseComponent,
-      },
-      {
-        path: 'my-courses',
-        component: MyCoursesComponent
-      },
-      {
-        path: 'detail-course/:id',
-        component: DetailCourseComponent,
-      },
-      {
-        path: 'detail-my-course/:id',
-        component: DetailMyCourseComponent
-      },
-      {
-        path: '',
-        component: HomepageComponent
-      },
-    ]
-  }
-];
-
+import { StudentContentRoutingModule } from './student-content-routing.module';
 
 @NgModule({
   declarations: [
@@ -60,10 +30,10 @@ const routes: Routes = [
     StudentService
   ],
   imports: [
-    RouterModule.forChild(routes),
     CommonModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    StudentContentRoutingModule
   ]
 })
 export class StudentContentModule { }
