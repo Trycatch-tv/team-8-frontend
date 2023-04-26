a5ce4efde4ea5d1750fa0771654d38aa8b8fcf0d
julian moreno <julianmoreno242@gmail.com>
2023-04-26 17:24:12 -0500

    Added login service student

--- src/app/services/student/student.service.ts
+++ src/app/services/student/student.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getAllCourses() {
     return this.http.get('http://localhost:8000/list_courses/');
+  }
+  loginStudent(data: any) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' })
+    const url = 'http://localhost:8000/student-login/'
+    return this.http.post(url, data, { headers })
   }
 }
