1db1619e662567d1e03a4e5ed45c9336c83ed3cf
julian moreno <julianmoreno242@gmail.com>
2023-04-26 09:51:21 -0500

    Added error variable

--- src/app/student-content/container-course/container-course.component.ts
+++ src/app/student-content/container-course/container-course.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from 'src/app/services/student/student.service';
-import { StudentData } from '../type';
+import { ErrorCourses, StudentData } from '../type';
+import { catchError, throwError } from 'rxjs';
 @Component({
   selector: 'app-container-course',
   templateUrl: './container-course.component.html',
@@ -11,23 +12,36 @@ export class ContainerCourseComponent implements OnInit {
   constructor(private studentService: StudentService) { }
   coursesAll!: Array<StudentData>
   courses!: Array<StudentData>
+  error: ErrorCourses={
+    message:'',
+    isError:false
+  }
 
 
   ngOnInit() {
-    this.studentService.getAllCourses().subscribe((data: any) => {
+    this.studentService.getAllCourses().pipe(
+      catchError((error: any) => {
+        this.error = {
+          message:'Ocurred an error: ' + error.name,
+          isError:true
+        }
+        return throwError('Ocurred an error')
+      })
+    ).subscribe((data: any) => {
       this.courses = data
       this.coursesAll = data
     });
+
   }
   handleSearch() {
     if (!this.inputSearchValue) {
       this.courses = this.coursesAll;
-      return;
+      return
     }
-  
-    this.courses=this.coursesAll.filter(item => (
+
+    this.courses = this.coursesAll.filter(item => (
       item.nombre.toLowerCase().includes(this.inputSearchValue.toLowerCase())
-    ));
+    ))
   }
 
 }
