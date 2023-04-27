bc1298383f88737ae7568fa7a439fd91fecbbbd7
julian moreno <julianmoreno242@gmail.com>
2023-04-27 12:27:59 -0500

    services teacher added

--- src/app/services/teacher/teacher.service.ts
+++ src/app/services/teacher/teacher.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TeacherService {
+export class TeacherService   {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getOneCourse(id:string){
+    return this.http.get(`http://localhost:8000/update_course/${id}/`);
+  } 
+
+  add_course(data:any){
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post('http://localhost:8000/add_course/',data,{headers}); 
+  }
+  update_course(data:any,id:string){
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.put(`http://localhost:8000/update_course/${id}/`,data,{headers}); 
+  }
 }
