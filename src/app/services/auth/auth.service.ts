9464770b788ce163750615972304a3689c15d94c
JhonTreft <jonz37133@gmail.com>
2023-04-27 14:50:12 -0500

    funcionality of auth in the admin-user

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+
+  /*
+  static isAuthenticated = false;
+
+
+  Auth() {
+     isAuthenticated=true;
+  }
+
+  logout() {
+     this.isAuthenticated=false;
+  }*/
+
+
+  static isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+
+  static login() {
+    localStorage.setItem('isAuthenticated', 'true');
+    this.isAuthenticated = true;
+    console.log(localStorage.getItem('isAuthenticated')); // Verificar el valor actual de 'isAuthenticated' en localStorage
+
+    return this.isAuthenticated
+  }
+
+  static logout() {
+    console.log("pasa logout")
+    localStorage.setItem('isAuthenticated', 'false');
+    this.isAuthenticated = false;
+
+    console.log(localStorage.getItem('isAuthenticated')); // Verificar el valor actual de 'isAuthenticated' en localStorage
+    return this.isAuthenticated
+  }
+
+
+
+
+}
