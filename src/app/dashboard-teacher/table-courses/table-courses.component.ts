d28c1dd679baf7e8bddcef42d54322f8ddaafde1
julian moreno <julianmoreno242@gmail.com>
2023-04-27 12:27:05 -0500

    variables and provider services added

--- src/app/dashboard-teacher/table-courses/table-courses.component.ts
+++ src/app/dashboard-teacher/table-courses/table-courses.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminService} from '../../services/admin/admin.service'
+import { MessageService } from 'primeng/api';
+
 @Component({
   selector: 'app-table-courses',
   templateUrl: './table-courses.component.html',
@@ -9,8 +11,10 @@ export class TableCoursesComponent implements OnInit {
 
   col:any;
   datos:any;
+  itemId:any;
 
-  constructor(private service_admin:AdminService){}
+  constructor(private service_admin:AdminService,private messageService: MessageService){}
+  
   ngOnInit(): void {
     this.service_admin.getlist_courses().subscribe((data:any) =>{
       //this.users = JSON.parse(data);
@@ -18,5 +22,25 @@ export class TableCoursesComponent implements OnInit {
       this.col = Object.keys(data[0]).slice(0,7)
     });
 
+  }
+
+  showConfirm2(id: any) {
+    this.messageService.add({key: 'confirm', sticky: true, severity:'warn', summary:'Estas Seguro?', detail:'Desea elimina este curso'});
+    this.itemId = id;
+  }
+
+  onConfirm2() {
+    this.messageService.clear('confirm');
+    this.service_admin.deleteCourse(this.itemId).subscribe((data:any)=>{
+      this.datos=this.datos.filter((item:any)=>item.id !== this.itemId)
+    })
+
+    // aquí puedes agregar la lógica que se ejecutará cuando el usuario haga clic en el botón "Yes"
+  }
+
+  onReject2() {
+    this.messageService.clear('confirm');
+
+    // aquí puedes agregar la lógica que se ejecutará cuando el usuario haga clic en el botón "No"
   }
 }
--- src/app/dashboard-teacher/table-students/table-students.component.ts
+++ src/app/dashboard-teacher/table-students/table-students.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AdminService} from '../../services/admin/admin.service';
 
 import {User} from '../../interface/students/students';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-table-students',
@@ -11,12 +12,13 @@ import {User} from '../../interface/students/students';
 export class TableStudentsComponent implements OnInit {
 
 
-  constructor(private service_admin:AdminService){}
+  constructor(private service_admin:AdminService,private messageService:MessageService){}
 
   users:User[] =[];
   col:any;
   datos:any;
-
+  itemId: any;
+  showModal = false;
 
   ngOnInit(): void {
     this.service_admin.getlist_students().subscribe((data:any) =>{
@@ -25,5 +27,29 @@ export class TableStudentsComponent implements OnInit {
       this.col = Object.keys(data[0]).slice(0,7)
     });
 
+  }
+  showConfirm1(id: any) {
+    this.messageService.add({key: 'confirm', sticky: true, severity:'warn', summary:'Estas Seguro?', detail:'Desea elimina este estudiante'});
+    this.itemId = id;
+  }
+
+  onConfirm1() {
+    this.messageService.clear('confirm');
+    this.service_admin.deleteStudent(this.itemId).subscribe((data:any)=>{
+      console.log(data)
+    })
+
+    // aquí puedes agregar la lógica que se ejecutará cuando el usuario haga clic en el botón "Yes"
+  }
+
+  onReject1() {
+    this.messageService.clear('confirm');
+
+    // aquí puedes agregar la lógica que se ejecutará cuando el usuario haga clic en el botón "No"
+  }
+
+
+  toggleModal(){
+    this.showModal = !this.showModal;
   }
 }
