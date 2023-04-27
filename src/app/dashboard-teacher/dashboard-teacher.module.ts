0cc68988554c1e8d423da6b9afcdc0cd760ba363
julian moreno <julianmoreno242@gmail.com>
2023-04-27 12:20:34 -0500

    Added new modules

--- src/app/dashboard-teacher/dashboard-teacher.module.ts
+++ src/app/dashboard-teacher/dashboard-teacher.module.ts
@@ -4,16 +4,28 @@ import { HomeComponent } from './home/home.component';
 import { TableStudentsComponent } from './table-students/table-students.component';
 import { TableCoursesComponent } from './table-courses/table-courses.component';
 import { DashboardTeacherRoutingModule } from './dashboard-teacher-routing.module';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateCourseComponent } from './update-course/update-course.component';
+import { DetailCourseComponent } from './detail-course/detail-course.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     TableStudentsComponent,
-    TableCoursesComponent
+    TableCoursesComponent,
+    AddCourseComponent,
+    UpdateCourseComponent,
+    DetailCourseComponent
   ],
   imports: [
     CommonModule,
     DashboardTeacherRoutingModule,
+    ToastModule,
+    ButtonModule,
+    FormsModule
   ]
 })
 export class DashboardTeacherModule { }
